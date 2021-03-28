ae10e64fd4d7194dde918d017c895338d8fdcd38
Stanford Rosenthal <nolastan@gmail.com>
2021-03-27 20:13:55 -0500

    fetch bulletin

--- functions/parse.js
+++ functions/parse.js
@@ -1,9 +1,32 @@
 exports.handler = async event => {
-  const url = event.queryStringParameters.url;
-  if(!url) { return "Please specify a URL" }
-  
-  return {
-    statusCode: 200,
-    body: `Fetching ${url}`,
-  }
+  const bulletin = event.queryStringParameters.bulletin;
+  const url = `https://content.govdelivery.com/accounts/LANOLA/bulletins/${bulletin}`;
+
+  const fetch = require("node-fetch");
+  var jsdom = require('jsdom');
+  const { JSDOM } = jsdom;
+
+    let request = await fetch(url);
+    let text = await request.text();
+
+    const dom = await new JSDOM(text);
+    html = dom.window.document.querySelector("#main-body").innerHTML;
+    html = `
+      <html>
+        <head>
+          <meta content="text/html; charset=UTF-8" http-equiv="Content-Type">
+        </head>
+        <body>
+          ${html}
+        </body>
+      </html>  
+    `
+
+    const response = {
+        statusCode: request.status,
+        body: html,
+        header: request.headers
+    };
+
+    return response;
 }
