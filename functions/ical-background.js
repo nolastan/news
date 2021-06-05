c52f80669902b3ae382e15e42c10d8014e3e40e2
Stanford Rosenthal <nolastan@gmail.com>
2021-06-05 18:38:56 -0500

    more post-debug cleanup

--- functions/ical-background.js
+++ functions/ical-background.js
@@ -1,9 +1,6 @@
 const ical = require('node-ical')
 const MongoClient = require('mongodb').MongoClient
-
-process.on('uncaughtException', function(err) {
-  console.log('Caught exception: ' + err);
-});
+const now = new Date()
 
 exports.handler = async event => {
   const connectionStr = `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@nola.uiwnl.mongodb.net?retryWrites=true&w=majority`
@@ -16,8 +13,8 @@ exports.handler = async event => {
   const db = client.db('nolatoday')
 
   let result = await getCalendars(db)
-    .then( async calendars => { return await importEvents(calendars) } )
-    .then( events => { return saveEvents(db, events) } )
+    .then( async calendars => await importEvents(calendars) )
+    .then( events => saveEvents(db, events) )
 
   return result
 }
@@ -28,7 +25,6 @@ async function getCalendars(db) {
       .collection('calendars')
       .find({format: 'ics'})
       .toArray()
-    console.log(`calendars: ${JSON.stringify(calendars)}`)
     resolve(calendars)
  })
 }
@@ -48,15 +44,12 @@ async function importEvents(calendars) {
 
 function processICS(data, venue) {
   let events = []
-  const now = new Date()
   for(const key in data) {
     const event = data[key]
-    if(event.type == 'VEVENT') {
-      if(new Date(event.start) > now && event.summary) {
-        event.venue = venue
-        events.push(event)
-        console.log(`Imported "${event.summary}" from ${venue}.`)
-      }
+    if(event.type == 'VEVENT' && new Date(event.start) > now && event.summary) {
+      event.venue = venue
+      events.push(event)
+      console.log(`Imported "${event.summary}" (${event.start}) from ${venue}.`)
     }
   }
   return events
